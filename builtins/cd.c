b015af52daa6a0f5c9fd45b139bae916724e6bab
Médéris Frambourt <mframbou@student.42nice.fr>
2021-11-22 16:40:21 +0100

    Fixed cd

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: mframbou <mframbou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 11:57:52 by mframbou          #+#    #+#             */
-/*   Updated: 2021/11/22 16:12:05 by mframbou         ###   ########.fr       */
+/*   Updated: 2021/11/22 16:38:24 by mframbou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,15 @@
 
 int	main(int argc, char *argv[])
 {
-	int	res;
+	int		res;
+	char	*home_path;
 
 	if (argc == 1)
-		res = chdir(getenv("HOME"));
+	{
+		home_path = getenv("HOME");
+		if (home_path)
+			res = chdir(home_path);
+	}
 	else
 		res = chdir(argv[1]);
 	if (res == -1)
