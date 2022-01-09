c626382aabb91d2048a1c8694e365af899db6449
Médéris Frambourt <mframbou@student.42nice.fr>
2022-01-09 16:03:32 +0100

    Added a function to check if the variable name is conform to POSIX standard

---
+++ parse/is_valid_bash_var.c
@@ -0,0 +1,23 @@
+
+#include "../minishell.h"
+
+/*
+	Bash variable names consist only of letters, digits or underscore,
+	and cannot start with a digit
+
+	- Check if first character is a digit
+	- If all other are either digits, alpha or '_', it's fine.
+*/
+int	is_valid_variable_name(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (ft_isdigit(str[i]))
+		return (0);
+	while (str[i] && (ft_isalpha(str[i]) || str[i] == '_' || ft_isdigit(str[i])))
+		i++;
+	if (str[i])
+		return (0);
+	return (1);
+}
