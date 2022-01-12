8daea9602c4f605af029b1e70104eeb689fe4a9a
Médéris Frambourt <mframbou@student.42nice.fr>
2022-01-12 18:31:29 +0100

    All builtins follow the norm, new header :)

--- builtins/environment/get_env.c
+++ builtins/environment/get_env.c
@@ -1,18 +1,59 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_env.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mframbou <mframbou@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/22 17:48:20 by mframbou          #+#    #+#             */
-/*   Updated: 2022/01/07 16:55:52 by mframbou         ###   ########.fr       */
-/*                                                                            */
+/*                       .-.                       .                          */
+/*                      / -'                      /                           */
+/*       .  .-. .-.   -/--).--..-.  .  .-. .-.   /-.  .-._.)  (               */
+/*        )/   )   )  /  /    (  |   )/   )   ) /   )(   )(    )         .    */
+/*   By: '/   /   (`.'  /      `-'-.-/   /.- (.''--'`-`-'  `--':        /     */
+/*                  -'            (   \  / .-._.).--..-._..  .-.  .-../ .-.   */
+/*   Created:   by                 `-' \/ (   )/    (   )  )/   )(   / (  |   */
+/*   Updated:   by                     /\  `-'/      `-'  '/   (  `-'-..`-'-' */
+/*                                 `._;  `._;                   `-            */
 /* ************************************************************************** */
 
 #include "environment.h"
 
 t_env_link	**get_var_list(void);
+
+static char	*generate_var_string(t_env_link *var)
+{
+	char	*res;
+	int		str_len;
+
+	str_len = ft_strlen(var->var.key) + 1 + ft_strlen(var->var.value) + 1;
+	res = (char *) malloc(sizeof(char) * str_len);
+	if (res)
+	{
+		ft_bzero(res, sizeof(char) * str_len);
+		ft_strlcat(res, var->var.key, str_len);
+		ft_strlcat(res, "=", str_len);
+		ft_strlcat(res, var->var.value, str_len);
+	}
+	return (res);
+}
+
+/*
+	We can use this because res[i] was set to NULL, marking the end
+	of our array (since it's always the last one that fails, because if
+	it does it returns)
+*/
+static void	*free_all_return_null(char **str_array)
+{
+	free_ft_split(str_array);
+	return (NULL);
+}
+
+static int	count_lst_size(t_env_link *start)
+{
+	int	i;
+
+	i = 0;
+	while (start)
+	{
+		i++;
+		start = start->next;
+	}
+	return (i);
+}
 
 /*
 	Strlen KEY = '='  + Strlen VALUE + '\0'
@@ -22,29 +63,19 @@ char	**get_env_as_string_array(void)
 	t_env_link	*curr;
 	char		**res;
 	int			i;
-	int			str_len;
 
 	i = 0;
 	curr = *get_var_list();
-	while (curr)
-	{
-		i++;
-		curr = curr->next;
-	}
-	res = (char **) malloc(sizeof(char *) * (i + 1));
+	res = (char **) malloc(sizeof(char *) * (count_lst_size(curr) + 1));
 	if (res)
 	{
 		i = 0;
 		curr = *get_var_list();
 		while (curr)
 		{
-			str_len = ft_strlen(curr->var.key) + 1 + \
-						ft_strlen(curr->var.value) + 1;
-			res[i] = (char *) malloc(sizeof(char) * str_len);
-			ft_bzero(res[i], sizeof(char) * str_len);
-			ft_strlcat(res[i], curr->var.key, str_len);
-			ft_strlcat(res[i], "=", str_len);
-			ft_strlcat(res[i], curr->var.value, str_len);
+			res[i] = generate_var_string(curr);
+			if (!res[i])
+				return (free_all_return_null(res));
 			i++;
 			curr = curr->next;
 		}
